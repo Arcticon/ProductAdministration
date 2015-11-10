33eea0cc42df9af6e0c6a8ec91c1d9940d253f44
Arcticon <arcticonhardstyle@gmail.com>
2015-11-10 12:16:57 +0100

    * Added variables and Constructors to Product

--- src/de/arcticon/products/Product.java
+++ src/de/arcticon/products/Product.java
@@ -1,5 +1,26 @@
 package de.arcticon.products;
 
 public class Product {
+	
+	private static short id;
+	private static String name;
+	private static float price;
+	private static short amount;
+	
+	public Product(){
+		
+	}
+	
+	public Product(short id, String name, float price, short amount){
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return this.id + "\t " + this.name + "\t " + this.price + "\t " + this.amount;
+	}
 
 }
